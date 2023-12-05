de4603ac0d9314e37d1556f995900fa4ed9dd693
Amelia Dutta <49182604+amelia2802@users.noreply.github.com>
2023-12-05 21:15:58 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,24 @@
+// data.js
+export const mythCreature = [
+  {
+    url: "https://i.pinimg.com/736x/77/18/1b/77181be3e91da2ba068240d4dfd559cf.jpg",
+  },
+  {
+    url: "https://static.wikia.nocookie.net/lovestruckvoltage/images/8/8a/Diego.jpg/revision/latest?cb=20180909190024",
+  },
+  {
+    url: "https://i.pinimg.com/736x/80/0d/99/800d9931802a5346ceb8aff42d27c236.jpg",
+  },
+  {
+    url: "https://c4.wallpaperflare.com/wallpaper/245/642/544/cartoon-cute-disney-fairy-wallpaper-preview.jpg",
+  },
+  {
+    url: "https://w0.peakpx.com/wallpaper/462/700/HD-wallpaper-werewolf-wolfman-moon-wolf-wolves.jpg",
+  },
+  {
+    url: "https://w0.peakpx.com/wallpaper/780/370/HD-wallpaper-zodiac-sagittarius-girl-sagittarius-warrior-art-archer-centaur-yasushi-matsuoka-planet-fantasy-zodiac-yasuartstudio-thumbnail.jpg",
+  },
+  {
+    url: "https://static.zerochan.net/Little.Mermaid.%28Disney%29.full.2847378.jpg",
+  },
+];
